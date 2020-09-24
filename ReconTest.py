aafebd0b2eabe3ad4a7fca451916ab184140872f
pengdada <chinhou0718@gmail.com>
2020-09-24 12:42:41 +0900

    complex->array

--- ReconTest.py
+++ ReconTest.py
@@ -31,17 +31,21 @@ coord_file = phantom_dir + 'coord.npy'
 # Choose computing device.
 # Device(-1) specifies CPU, while others specify GPUs.
 # GPU requires installing cupy.
-try:
-    device = sp.Device(0)
-except:
-    device = sp.Device(-1)
+#try:
+#    device = sp.Device(0)
+#except:
+#    device = sp.Device(-1)
+
+device = sp.Device(-1)
 
 xp = device.xp
 device.use()
 
 # Load datasets.
 coord = xp.load(coord_file)
+
 ksp = xp.load(ksp_file)
+ksp = np.stack((ksp.real, ksp.imag), axis=-1)
 
 #mps = mr.app.JsenseRecon(ksp, coord=coord, device=device).run()
 mps = mr.app.JsenseRecon(ksp, device=device).run()
